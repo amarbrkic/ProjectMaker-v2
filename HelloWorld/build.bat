af0e3cb258124a09c98af45686d85f6a53765221
Amar Brkic <amar.brkic202@gmail.com>
2023-07-27 21:03:55 +0200

    Cleaned up

--- HelloWorld/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-MSBuild HelloWorld.vcxproj -property:Configuration=Debug -property:Platform=x64
-bin\HelloWorld.exe
--- src/network.py
+++
@@ -1,9 +0,0 @@
-import urllib
-
-
-def connect(host='http://google.com'):
-    try:
-        urllib.request.urlopen(host)
-        return True
-    except:
-        return False
